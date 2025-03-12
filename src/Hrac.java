2a80560b4aa3197a4938b7befb0483173c339670
parkoss <marek.sedlacek73@seznam.cz>
2025-03-12 22:48:48 +0100

    ve tride hrac se zapisuji hodnoty hrace

---
+++ src/Hrac.java
@@ -0,0 +1,25 @@
+public class Hrac {
+    private int zivoty;
+    private int sila;
+
+    public Hrac(int zivoty, int sila) {
+        this.zivoty = zivoty;
+        this.sila = sila;
+    }
+
+    public int getZivoty() {
+        return 50;
+    }
+
+    public int getSila() {
+        return 10;
+    }
+
+    public void setSila(int sila) {
+        this.sila = sila;
+    }
+
+    public void setZivoty(int zivoty) {
+        this.zivoty = zivoty;
+    }
+}
